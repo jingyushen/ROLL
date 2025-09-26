17e4b21c4e6bb8f54b8a6065dfbacdfe4fbd1eb8
fengling <shenjingyu.sjy@alibaba-inc.com>
2025-09-26 11:18:04 +0000

    feat: update single node script

--- examples/agentic_demo/run_agentic_pipeline_frozen_lake_single_node_demo.sh
+++ examples/agentic_demo/run_agentic_pipeline_frozen_lake_single_node_demo.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set +x
 
-ROLL_PATH="/workspace/ROLL-main"
+ROLL_PATH="/workspace/ROLL"
 CONFIG_PATH=$(basename $(dirname $0))
 export PYTHONPATH="$ROLL_PATH:$PYTHONPATH"
-python examples/start_agentic_pipeline.py --config_path $CONFIG_PATH  --config_name agent_val_frozen_lake_single_node_demo
+export MODEL_DOWNLOAD_TYPE="MODELSCOPE"
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+/usr/bin/python /workspace/ROLL/examples/start_agentic_pipeline.py --config_path $CONFIG_PATH  --config_name agent_val_frozen_lake_single_node_demo
